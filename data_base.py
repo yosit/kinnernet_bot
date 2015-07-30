3ef22fdeaf0ea9951740e9aed8c61fabfa3ead25
Yosi Taguri <yosi@taguri.com>
2015-07-30 21:41:02 +0300

    more changes

---
+++ data_base.py
@@ -0,0 +1,362 @@
+db_filename = "board.json"
+
+sessions = [
+    {
+        "start_time": "2015-07-30 18:00:00",
+        'duration': 90,
+        "speaker": "",
+        "title": "Intros",
+        "location": "Academy"
+    },
+    {
+        "start_time": "2015-07-30 19:30:00",
+        'duration': 30,
+        "speaker": "",
+        "title": "Preview",
+        "location": "Academy"
+    },
+    {
+        "start_time": "2015-07-30 20:00:00",
+        'duration': 60,
+        "speaker": "",
+        "title": "Open BBQ and networking",
+        "location": "Academy"
+    },
+    {
+        "start_time": "2015-07-30 21:00:00",
+        'duration': 60,
+        "speaker": "",
+        "title": "Gadgethon + songs + werewolf + music",
+        "location": "Academy"
+    },
+    # Friday
+    {
+        "start_time": "2015-07-31 08:00:00",
+        'duration': 60,
+        "speaker": "",
+        "title": "Breakfast",
+        "location": ""
+    },
+    {
+        "start_time": "2015-07-31 09:00:00",
+        'duration': 240,
+        "speaker": "",
+        "title": "Offsite and H-Farm activity",
+        "location": ""
+    },
+    {
+        "start_time": "2015-07-31 05:00:00",
+        'duration': 240,
+        "speaker": "GIORGIO",
+        "title": "CANOE",
+        "location": "LAGOON"
+    },
+    {
+        "start_time": "2015-07-31 9:00:00",
+        'duration': 240,
+        "speaker": "MARTA",
+        "title": "ARTISTIC PERFORMANCE AND INSTALLATION",
+        "location": "H-FARM"
+    },
+    {
+        "start_time": "2015-07-31 9:00:00",
+        'duration': 240,
+        "speaker": "FRANCESCO",
+        "title": "TESSITURA BEVILACQUA",
+        "location": "VENICE"
+    },
+    {
+        "start_time": "2015-07-31 9:00:00",
+        'duration': 240,
+        "speaker": "ALESSANDRO B",
+        "title": "VETRERIA ORSONI",
+        "location": "VENICE"
+    },
+    {
+        "start_time": "2015-07-31 7:00:00",
+        'duration': 240,
+        "speaker": "CHIARA",
+        "title": "RUNNING",
+        "location": "H-FARM"
+    },
+    {
+        "start_time": "2015-07-31 9:00:00",
+        'duration': 240,
+        "speaker": "ALESSANDRA",
+        "title": "YOGA/MEDITATION",
+        "location": "H-FARM"
+    },
+    {
+        "start_time": "2015-07-31 10:00:00",
+        'duration': 240,
+        "speaker": "MARTA",
+        "title": "DRAWING/PHOTO WORKSHOP",
+        "location": "H-FARM"
+    },
+    {
+        "start_time": "2015-07-31 10:00:00",
+        'duration': 240,
+        "speaker": "BENEDETTO",
+        "title": "MANI N PASTA",
+        "location": "H-FARM"
+    },
+
+    {
+        "start_time": "2015-07-31 13:00:00",
+        'duration': 60,
+        "speaker": "",
+        "title": "lunch",
+        "location": "Green House"
+    },
+    {
+        "start_time": "2015-07-31 14:30:00",
+        'duration': 60,
+        "speaker": "MATAN MELAMED",
+        "title": "Solving the venetion mosquitto problem + other fun engineering- workshop",
+        "location": "Serra"
+    },
+    {
+        "start_time": "2015-07-31 14:30:00",
+        'duration': 60,
+        "speaker": "Liat Segal",
+        "title": "ART & TECH SHOWCASE",
+        "location": "Convivium"
+    },
+    {
+        "start_time": "2015-07-31 14:30:00",
+        'duration': 60,
+        "speaker": "JIM & KATZ",
+        "title": "FROM THE POWERFUL ME TO THE POWERFUL US",
+        "location": "Industry / Officina"
+    },
+    {
+        "start_time": "2015-07-31 14:30:00",
+        'duration': 60,
+        "speaker": "HCAMPERS",
+        "title": "3 MIN PITCHES FROM H-STARTUPS",
+        "location": "H-Camp"
+    },
+    {
+        "start_time": "2015-07-31 14:30:00",
+        'duration': 60,
+        "speaker": "DROR GILL",
+        "title": "360 VIDEOS: THE PHYSICAL VR",
+        "location": "Digital Accademia"
+    },
+    {
+        "start_time": "2015-07-31 14:30:00",
+        'duration': 60,
+        "speaker": "PETER VAUGHN",
+        "title": "SMART ADAPTIVE EMERGENT PLAYFUL CITIES THAT WORK",
+        "location": "Giardino"
+    },
+
+    {
+        "start_time": "2015-07-31 15:30:00",
+        'duration': 60,
+        "speaker": "MEL ROSENBERG",
+        "title": "STORY SONGS AND SPIRTIS",
+        "location": "Serra"
+    },
+    {
+        "start_time": "2015-07-31 15:30:00",
+        'duration': 60,
+        "speaker": "LIMOR LAHIANI",
+        "title": "DIVINE TECHNOLOGY",
+        "location": "Convivium"
+    },
+    {
+        "start_time": "2015-07-31 15:30:00",
+        'duration': 60,
+        "speaker": "BRAD TEMPLETON",
+        "title": "THE FUTURE OF CREATING $ FROM CREATIVITY WHEN COPYRIGHT IS DEAD",
+        "location": "Industry / Officina"
+    },
+    {
+        "start_time": "2015-07-31 15:30:00",
+        'duration': 60,
+        "speaker": "TBD",
+        "title": "EMERGENT FORMS OF INNOVATION + MAKER COMMUNITIES",
+        "location": "H-Camp"
+    },
+    {
+        "start_time": "2015-07-31 15:30:00",
+        'duration': 60,
+        "speaker": "SRIDHAR LYENGER",
+        "title": "HOW STARTING A BAND IS LIKE FORMING A STARTUP",
+        "location": "Digital Accademia"
+    },
+    {
+        "start_time": "2015-07-31 15:30:00",
+        'duration': 60,
+        "speaker": "TBD",
+        "title": "Section II",
+        "location": "Giardino"
+    },
+
+    {
+        "start_time": "2015-07-31 17:00:00",
+        'duration': 60,
+        "speaker": "MENNY",
+        "title": "POWER POINT ADVANCED TRICKS",
+        "location": "Serra"
+    },
+    {
+        "start_time": "2015-07-31 17:00:00",
+        'duration': 60,
+        "speaker": "TBD",
+        "title": "Section III",
+        "location": "Convivium"
+    },
+    {
+        "start_time": "2015-07-31 17:00:00",
+        'duration': 60,
+        "speaker": "ROY RAMON",
+        "title": "SECURITY IN THE IOT ERA",
+        "location": "Industry / Officina"
+    },
+    {
+        "start_time": "2015-07-31 17:00:00",
+        'duration': 60,
+        "speaker": "AMI BEN BASSAT",
+        "title": "CURIOSITY CIRCLE: WHERE IS EVERYBODY/THE FERMI PARADOX",
+        "location": "H-Camp"
+    },
+    {
+        "start_time": "2015-07-31 17:00:00",
+        'duration': 60,
+        "speaker": "TBD",
+        "title": "THE DIGITAL ACADEMIA CULTUR HOTSPOT",
+        "location": "Digital Accademia"
+    },
+    {
+        "start_time": "2015-07-31 17:00:00",
+        'duration': 60,
+        "speaker": "TBD",
+        "title": "Section III",
+        "location": "Giardino"
+    },
+    {
+        "start_time": "2015-07-31 18:00:00",
+        'duration': 60,
+        "speaker": "",
+        "title": "One hour big idea",
+        "location": "Academy"
+    },
+    {
+        "start_time": "2015-07-31 19:00:00",
+        'duration': 90,
+        "speaker": "",
+        "title": "Gala dressup",
+        "location": "shower fast"
+    },
+    {
+        "start_time": "2015-07-31 20:30:00",
+        'duration': 120,
+        "speaker": "",
+        "title": "Gala On board",
+        "location": "Boat"
+    },
+    {
+        "start_time": "2015-07-31 23:00:00",
+        'duration': 120,
+        "speaker": "",
+        "title": "Bus",
+        "location": "Bus ride back"
+    },
+
+    # Saturday
+    {
+        "start_time": "2015-08-01 08:00:00",
+        'duration': 60,
+        "speaker": "",
+        "title": "Breakfast",
+        "location": "Academy"
+    },
+    {
+        "start_time": "2015-07-31 7:00:00",
+        'duration': 240,
+        "speaker": "CHIARA",
+        "title": "RUNNING",
+        "location": "H-FARM"
+    },
+    {
+        "start_time": "2015-08-01 09:00:00",
+        'duration': 240,
+        "speaker": "ALESSANDRO A.",
+        "title": "VISIT TO CASA VECCIA BREWERY",
+        "location": "TREVISO"
+    },
+    {
+        "start_time": "2015-07-31 9:00:00",
+        'duration': 240,
+        "speaker": "ALESSANDRA",
+        "title": "YOGA/MEDITATION",
+        "location": "H-FARM"
+    },
+    {
+        "start_time": "2015-08-01 10:00:00",
+        'duration': 240,
+        "speaker": "FRANCESCA",
+        "title": "TIRAMISU",
+        "location": "H-FARM"
+    },
+    {
+        "start_time": "2015-08-01 10:00:00",
+        'duration': 240,
+        "speaker": "MARTA",
+        "title": "DRAWING/PHOTO WORKSHOP",
+        "location": "H-FARM"
+    },
+    {
+        "start_time": "2015-08-01 10:00:00",
+        'duration': 240,
+        "speaker": "BEATRICE",
+        "title": "SINGING WORKSHOP",
+        "location": "H-FARM"
+    },
+    {
+        "start_time": "2015-08-01 10:00:00",
+        'duration': 60,
+        "speaker": "THOMSON",
+        "title": "BEYOND THE TRANSACTION:ANATOMY OF THE ARTISAN ECONOMY",
+        "location": "H-FARM"
+    },
+    {
+        "start_time": "2015-08-01 10:00:00",
+        'duration': 60,
+        "speaker": "JEREMIE",
+        "title": "DISCUSS APPLE WATCH USAGE (OWNERS ONLY BUT YOU CAN COME IF YOU PLAN TO BUY ONE)",
+        "location": "H-FARM"
+    },
+    {
+        "start_time": "2015-08-01 10:00:00",
+        'duration': 60,
+        "speaker": "CLIPPINGER",
+        "title": "BLOCK THE CHAIN ID",
+        "location": "H-FARM"
+    },
+    {
+        "start_time": "2015-08-01 10:00:00",
+        'duration': 60,
+        "speaker": "DAVID-BARNETT",
+        "title": "DIGITAL MEANS TO LEVEL GLOBAL INEQUALITY",
+        "location": "H-FARM"
+    },
+    {
+        "start_time": "2015-08-01 13:00:00",
+        'duration': 60,
+        "speaker": "",
+        "title": "lunch",
+        "location": "Green House"
+    },
+    # Sunday
+    {
+        "start_time": "2015-08-02 08:00:00",
+        'duration': 60,
+        "speaker": "",
+        "title": "Breakfast",
+        "location": "Academy"
+    },
+
+]
